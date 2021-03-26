03c6b3763241aa0257dcdac70fb201a13bc934c4
zgkaii <2488784449@qq.com>
2021-03-26 16:52:19 +0800

    update 2021/03/26

---
+++ week02-nio/src/main/java/com/zgkaii/nio/mission/misision5/HttpServer2.java
@@ -0,0 +1,43 @@
+package com.zgkaii.nio.mission.misision5;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * @Author: Mr.Z
+ * @DateTime: 2021/03/26 16:08
+ * @Description: 多线程Socket程序
+ */
+public class HttpServer2 {
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(8802);
+        while (true) {
+            try {
+                Socket socket = serverSocket.accept();
+                new Thread(()->{
+                    service(socket);
+                }).start();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static void service(Socket socket) {
+        try {
+            PrintWriter printWriter = new PrintWriter(socket.getOutputStream(), true);
+            printWriter.println("HTTP/1.1 200 OK");
+            printWriter.println("Content-Type:text/html;charset=utf-8");
+            String body ="hello,nio2";
+            printWriter.println("Content-length:"+body.getBytes().length);
+            printWriter.println();
+            printWriter.write(body);
+            printWriter.close();
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ week02-nio/src/main/java/com/zgkaii/nio/mission/misision5/netty/HttpHandler.java
@@ -0,0 +1,80 @@
+package com.zgkaii.nio.mission.misision5.netty;
+
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.handler.codec.http.DefaultFullHttpResponse;
+import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.handler.codec.http.FullHttpResponse;
+import io.netty.handler.codec.http.HttpHeaderUtil;
+import io.netty.util.ReferenceCountUtil;
+
+import static io.netty.handler.codec.http.HttpHeaderValues.KEEP_ALIVE;
+import static io.netty.handler.codec.http.HttpHeaderNames.CONNECTION;
+import static io.netty.handler.codec.http.HttpResponseStatus.NO_CONTENT;
+import static io.netty.handler.codec.http.HttpResponseStatus.OK;
+import static io.netty.handler.codec.http.HttpVersion.HTTP_1_1;
+
+/**
+ * @Author: Mr.Z
+ * @DateTime: 2021/03/26 16:37
+ * @Description:
+ */
+public class HttpHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) {
+        ctx.flush();
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        long startTime = System.currentTimeMillis();
+        try {
+            //logger.info("channelRead流量接口请求开始，时间为{}", startTime);
+            FullHttpRequest fullRequest = (FullHttpRequest) msg;
+            String uri = fullRequest.uri();
+            //logger.info("接收到的请求url为{}", uri);
+
+            if (uri.contains("/test")) {
+                handlerTest(fullRequest, ctx, "Hello,Mr.Z!");
+            } else {
+                handlerTest(fullRequest, ctx, "Hello,Everyone!");
+            }
+        } finally {
+            ReferenceCountUtil.release(msg);
+        }
+        //logger.info("channelRead流量接口请求结束，时间为{}", System.currentTimeMillis() - startTime);
+    }
+
+    private void handlerTest(FullHttpRequest fullRequest, ChannelHandlerContext ctx, String body) {
+        //long startTime = System.currentTimeMillis();
+        FullHttpResponse response = null;
+        try {
+            String value = body;
+            response = new DefaultFullHttpResponse(HTTP_1_1, OK, Unpooled.wrappedBuffer(value.getBytes("UTF-8")));
+            response.headers().set("Content-Type", "application/json");
+            response.headers().setInt("Content-Length", response.content().readableBytes());
+
+        } catch (Exception e) {
+            System.out.println("处理测试接口出错" + e.getMessage());
+            response = new DefaultFullHttpResponse(HTTP_1_1, NO_CONTENT);
+        } finally {
+            if (fullRequest != null) {
+                if (!HttpHeaderUtil.isKeepAlive(fullRequest)) {
+                    ctx.write(response).addListener(ChannelFutureListener.CLOSE);
+                } else {
+                    response.headers().set(CONNECTION, KEEP_ALIVE);
+                    ctx.write(response);
+                }
+            }
+        }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
